93250625291553c4a9a83b1ef72fcbeb26cf056a
Kristofer Riedman <riedmank@hotmail.com>
2018-10-04 09:44:14 -0700

    refactored code to look cleaner

--- code-challenge-solutions/Program.cs
+++ code-challenge-solutions/Program.cs
@@ -13,29 +13,18 @@ namespace code_challenge_solutions
         {
             Console.WriteLine("Please provide 5 numbers between 1 and 10.");
             int[] array = new int[5];
-            int userInput = 0;
-            int target = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                userInput = Int32.Parse(Console.ReadLine());
-                if (userInput > 0 && userInput < 11)
-                {
-                    array[i] = userInput;
-                    Console.WriteLine($"Slot {i}: {array[i]}");
-                }
-                else
-                {
-                    Console.WriteLine("You did pick a valid number. Try again.");
-                    i--;
-                }
+                array[i] = NumberGetter();
+                Console.WriteLine($"Slot {i}: {array[i]}");
             }
             Console.WriteLine("Select a number to see if its in the array.");
-            target = Int32.Parse(Console.ReadLine());
-            ScoreCalc(array, target);
+            ScoreCalc(array, NumberGetter());
         }
 
         static void ScoreCalc(int[] arr, int target)
         {
+            //Store userInput
             int userTarget = target;
             int[] userArray = arr;
             int counter = 0;
@@ -47,6 +36,21 @@ namespace code_challenge_solutions
                 }
             }
             Console.WriteLine($"User Score: {counter * userTarget}.");
+        }
+
+        static int NumberGetter()
+        {
+            int userInput = 0;
+            bool result = true;
+            while(!(userInput > 0 && userInput < 11) || !result)
+            {
+                result = int.TryParse(Console.ReadLine(), out userInput);
+                if(!(userInput > 0 && userInput < 11) || !result)
+                {
+                    Console.WriteLine("You have picked an invalid number. Please try again.");
+                }
+            }
+            return userInput;
         }
     }
 }
