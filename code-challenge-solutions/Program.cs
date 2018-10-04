51c337f6e8060f14300f0ae207ee993da6b40fec
Kristofer Riedman <riedmank@hotmail.com>
2018-10-04 09:58:47 -0700

    added comments

--- code-challenge-solutions/Program.cs
+++ code-challenge-solutions/Program.cs
@@ -6,25 +6,27 @@ namespace code_challenge_solutions
     {
         static void Main(string[] args)
         {
-            InputGetter();
+            ArrayMaxResult();
         }
 
-        static void InputGetter()
+        static void ArrayMaxResult()
         {
             Console.WriteLine("Please provide 5 numbers between 1 and 10.");
             int[] array = new int[5];
             for (int i = 0; i < array.Length; i++)
             {
+                //Request userInput and insert into array
                 array[i] = NumberGetter();
                 Console.WriteLine($"Slot {i}: {array[i]}");
             }
             Console.WriteLine("Select a number to see if its in the array.");
+            //Pass user array and user target to ScoreCalc method
             ScoreCalc(array, NumberGetter());
         }
 
         static void ScoreCalc(int[] arr, int target)
         {
-            //Store userInput
+            //Store userInput from ArrayMaxResult method
             int userTarget = target;
             int[] userArray = arr;
             int counter = 0;
@@ -40,6 +42,7 @@ namespace code_challenge_solutions
 
         static int NumberGetter()
         {
+            //Requests userInput and validates response
             int userInput = 0;
             bool result = true;
             while(!(userInput > 0 && userInput < 11) || !result)
@@ -50,6 +53,7 @@ namespace code_challenge_solutions
                     Console.WriteLine("You have picked an invalid number. Please try again.");
                 }
             }
+            //Return valid response
             return userInput;
         }
     }
